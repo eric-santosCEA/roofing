cbe33580b43f067ba0ee258becfe38b7c8c551e8
Eric Santos <e.santos@joincea.com>
2021-05-19 13:13:26 -0600

    styled hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import '../styles/home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div className="hero">
+        <div className="left">
+          <h1>Cea Roofing</h1>
+          <p>We are the Clean Energy America roofing division</p>
+          <button>See Our Best Work</button>
+        </div>
+        <div className="right">
+          <img src="./photos/roofing-main-small.webp" alt="roofing" />
+        </div>
+      </div>
+    </>
   )
 }
 export default Home
